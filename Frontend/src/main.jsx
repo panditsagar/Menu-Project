41ceaafa313032d4d23a75612f2478f7397c4d0b
sagar kumar <panditsagar4995@gmail.com>
2025-05-27 02:28:47 +0530

    Revert "vercel setup"
    This reverts commit 8d90af3d20f107a42020f3bd924f523fe46a6db1.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { ToastContainer } from 'react-toastify'
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
+import { persistor, store } from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
