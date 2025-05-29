0bcf9694d83197ed506c0f565da9750ee45629fb
sagar kumar <panditsagar4995@gmail.com>
2025-05-30 00:32:13 +0530

    add

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
 import { ToastContainer } from 'react-toastify'
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store.js';
+import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
