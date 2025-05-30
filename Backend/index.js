380b48de644fad9fad3de20e996ec2d3e08ba8b6
sagar kumar <panditsagar4995@gmail.com>
2025-05-30 23:02:11 +0530

    add front url

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://menu-project-8ilo.vercel.app/",
+    origin: "https://menu-project-theta.vercel.app/",
     credentials: true,
   })
 );
