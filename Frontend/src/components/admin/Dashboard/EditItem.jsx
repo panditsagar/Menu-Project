d7cf340e12bc6e050bfa06ee24adf3d2c996cff3
sagar kumar <panditsagar4995@gmail.com>
2025-05-30 23:15:21 +0530

    Revert "add backend url"
    This reverts commit 28724672280ae255e8725341626a72e1e58755ba.

--- Frontend/src/components/admin/Dashboard/EditItem.jsx
+++ Frontend/src/components/admin/Dashboard/EditItem.jsx
@@ -74,7 +74,7 @@ export default function AddItem() {
         }
 
         try {
-            const res = await axios.put(`https://menu-project-v4c5.vercel.app/api/v1/items/update/${id}`, formData, {
+            const res = await axios.put(`https://menu-project-48xu.vercel.app/api/v1/items/update/${id}`, formData, {
                 headers: { "Content-Type": "multipart/form-data" },
                 withCredentials: true,
             });
