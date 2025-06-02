f7fd4b97edcbf0fdab618188293280d58fc70dce
sagar kumar <panditsagar4995@gmail.com>
2025-06-02 13:46:55 +0530

    fix err

--- Backend/controllers/admin.controllers.js
+++ Backend/controllers/admin.controllers.js
@@ -30,7 +30,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpsOnly: true,
-        sameSite: "none",
+        sameSite: "strict",
       })
       .json({
         message: "Welcome Back",
