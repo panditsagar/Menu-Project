f6c5cfbcf8160f6c2533136bf81e7af33914d86f
Sagar kumar <panditsagar4995@gmail.com>
2025-11-25 17:33:17 +0530

    c

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { ToastContainer } from 'react-toastify'
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
+import { persistor, store } from './redux/Store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
